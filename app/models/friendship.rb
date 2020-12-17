61ab774f87431f31bd968200bd72615034939c71
mke2111 <roymke134@gmail.com>
2020-12-17 20:14:07 +0300

    update friendship model

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,13 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  self.primary_key = :user_id, :friend_id
+
+  def confirm_friend
+    update_attributes(confirmed: true)
+    Friendship.create!(friend_id: user_id,
+                       user_id: friend_id,
+                       confirmed: true)
+  end
 end
